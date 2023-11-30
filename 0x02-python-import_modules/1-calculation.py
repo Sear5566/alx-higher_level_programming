0cca3a620e3da7ca98231080479725d407eb3238
Sear5566 <dwdsegun63@gmail.com>
2023-11-30 23:14:58 +0000

    updating the division of 1-calculation.py file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
 	b = 5
 	from calculator_1 import add, sub, mul, div
 	print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-	print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 	print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 	print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+	print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
