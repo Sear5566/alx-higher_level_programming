c19584cc11c68df7b9c19cc699667d2811285714
Sear5566 <dwdsegun63@gmail.com>
2023-12-02 10:05:42 +0000

    updated indentation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-	import sys
-	sum_arg = 0
-	for i in sys.argv[1:]:
-		sum_arg += int(i)
-	print("{:d}".format(sum_arg))
+    import sys
+    sum_arg = 0
+    for i in sys.argv[1:]:
+        sum_arg += int(i)
+    print("{:d}".format(sum_arg))
