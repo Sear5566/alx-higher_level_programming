ffca16f44b77bfa8e4fa1806ab5bf1349262ce0f
Sear5566 <dwdsegun63@gmail.com>
2023-12-10 01:15:46 +0000

    added conditions to last_digit.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    last_numb = number % 10
+else:
+    last_numb = number % -10
+print("last digit of {} is ".format(number), end = "")
+
+if last_numb > 5:
+    print("{} and is greater than 5".format(last_numb))
+if last_numb < 6 and last_numb != 0:
+    print("{} and is less than 6 and not 0".format(last_numb))
+if last_numb == 0:
+    print("{} and is 0".format(last_numb))
