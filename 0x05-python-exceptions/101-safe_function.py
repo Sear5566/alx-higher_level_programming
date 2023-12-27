ca0f26db0505dadc5b49e9973f7f5454d48d2864
Sear5566 <dwdsegun63@gmail.com>
2023-12-27 12:57:22 +0000

    new files added to python exceptions task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    strmen ="Exception: "
+    try:
+        result = fct(*args)  # passing arguments to function
+        return result
+    except (ZeroDivisionError, IndexError) as men:
+        strmen += str(men)
+        strmen += "\n"
+        sys.stderr.write(strmen)
+        return None
