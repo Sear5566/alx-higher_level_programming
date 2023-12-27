e25e15b599faa20567d39cec5aa9207d1de54e49
Sear5566 <dwdsegun63@gmail.com>
2023-12-27 15:41:56 +0000

    added permissions to all files

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    weight = 0
+    score_weight = 0
+    if my_list:
+        for tuples in my_list:
+            nume += (tuples[0] * tuples[1])
+            den += tuples[1]
+        return (score_weight / weight)
+    return (0)
