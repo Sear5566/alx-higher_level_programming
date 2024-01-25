c1e6f2a6baf089345dc0185aa956ecd15034e491
Sear5566 <dwdsegun63@gmail.com>
2024-01-25 10:13:59 +0000

    updated pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,12 +2,13 @@
 """Module that read a file and print your content
 """
 
-def write_file(filename="", text=""):
-        """Function that write a file
-        Args:
-            filename (str, optional): path file. Defaults to "".
-            text (str, optional): content. Defaults to "".
-        """
 
-        with open(filename, mode='w', encoding='utf-8') as afile:
+def write_file(filename="", text=""):
+    """Function that write a file
+    Args:
+        filename (str, optional): path file. Defaults to "".
+        text (str, optional): content. Defaults to "".
+    """
+
+    with open(filename, mode='w', encoding='utf-8') as afile:
                 return (afile.write(text))
