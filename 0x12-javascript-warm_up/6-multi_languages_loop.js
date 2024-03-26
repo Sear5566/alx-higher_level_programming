3a1fae7adb7eacc6bebcf1e8327b98ff92d748a4
Sear5566 <dwdsegun63@gmail.com>
2024-03-26 16:01:22 +0000

    edited typo error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const multi = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const language in multi) {
   console.log(`${multi[language]}`);
