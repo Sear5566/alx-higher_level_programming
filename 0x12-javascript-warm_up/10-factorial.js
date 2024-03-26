dd690500e5327e44cdec97b55d0ecf225332336c
Sear5566 <dwdsegun63@gmail.com>
2024-03-26 15:29:11 +0000

    removed whitespaces on factorial file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,15 +2,15 @@
 
 function factorial (n) {
     if (n === 0) {
-      return 1;
-    }
-    return n * factorial(n - 1);
+    return 1;
   }
-  
-  const ag = parseInt(process.argv[2], 10);
-  
-  if (!ag) {
-    console.log(1);
-  } else {
-    console.log(factorial(ag));
-  }
+  return n * factorial(n - 1);
+}
+
+const ag = parseInt(process.argv[2], 10);
+
+if (!ag) {
+  console.log(1);
+} else {
+  console.log(factorial(ag));
+}
